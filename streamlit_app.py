a36487df8f9ac1cf1326d2d08b975d102692f834
narutouzumaki <maiti.atin@gmail.com>
2025-03-19 19:16:55 -0400

    Converted to side bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,10 +72,6 @@ hide_streamlit_style = """
         .css-1d391kg .css-17eq0hr {
             background-color: orange;
         }
-        .stApp {
-            background-color: #D3D3D3;  /* Light gray color */
-        }
-        
         [data-testid="stSidebar"] {
             background-color: #FFA500;  /* Orange color */
         }
