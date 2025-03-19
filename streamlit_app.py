466cb8494186073bcc0649b0715214a7b718e870
narutouzumaki <maiti.atin@gmail.com>
2025-03-19 16:42:44 -0400

    init main

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Welcome to Our Wedding")
+st.header("Join Us on Our Special Day")
+st.write("We are excited to invite you to our wedding celebration!")
+# Add more sections as needed
