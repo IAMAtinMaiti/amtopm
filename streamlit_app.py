4c1f476c3519850f7576a15a0da64ae9a257afa0
narutouzumaki <maiti.atin@gmail.com>
2025-03-19 19:00:59 -0400

    Converted to side bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Sidebar navigation
 #selection = st.sidebar.selectbox("Navigation", ["Home", "Testimonials", "Gallery", "Event Timeline"])
-selection = option_menu("Navigation",["Home", "Testimonials", "Gallery", "Event Timeline"])
+selection = option_menu("Navigation",["Home", "Testimonials", "Gallery", "Event Timeline"], menu_icon="cast", default_index=0)
 
 
 # Display content based on selection
